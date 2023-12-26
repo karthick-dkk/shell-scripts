64b9c03b7d0eaa3c2595fd83b1908f1b0b543c6e
karthick-dkk <128692563+karthick-dkk@users.noreply.github.com>
2023-12-26 14:50:58 +0530

    Update monitor_CPU_RAM_DISK.sh

--- monitor_CPU_RAM_DISK.sh
+++ monitor_CPU_RAM_DISK.sh
@@ -1,18 +1,25 @@
+# Create a scritp to Monitor the CPU, RAM, DISK 
+# write a log if only reaches 80% 
+# Upload the log file to SFTP 
+# we will add this script to crontab and run every 3 mins
+#
+
+
 #/bin/bash 
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
 DATE=$(date "+%Y-%m-%d")
 THRESHOLD1=80 # Alert Percentage only for CPU 
 THRESHOLD2=80 # Alert Percentage only for RAM
-CLIENT="44_server"
+CLIENT="server_name"
 IP=`hostname -I | awk '{print $1}'`
 log="/tmp/logs/monitor_${CLIENT}_${IP}_${DATE}.log"
 
 # SFTP Server details
-SFTP_USER="Linux_Mate"
-SFTP_HOST="103.239.139.126"
-SFTP_PORT="5726"
+SFTP_USER="sftp_user"
+SFTP_HOST="192.168.111.1"
+SFTP_PORT="22"
 REMOTE_DIR="/upload/logs/"
-SFTP_PASS="Cart@123"
+SFTP_PASS="pass@123"
 # Local file to upload
 LOCAL_FILE="/tmp/logs/*.log"
 
@@ -47,7 +54,8 @@ function show_cpu_usage() {
 }
 
 function show_disk_usage() {
-    disk=`df -h | awk 'NR>1{print $6, $2, $4,$5}' | awk 'match($0, /([0-9.]+)%/) {percentage = substr($0, RSTART, RLENGTH - 1); if (percentage >= 50) printf "DISK: %s, Total: %s, Avail: %s, Used: %s \n ", $1, $2, $3, $4}' | sort -h |  grep -Eiv "run"`
+    disk=`df -h | awk 'NR>1{print $6, $2, $4,$5}' | awk 'match($0, /([0-9.]+)%/) {percentage = substr($0, RSTART, RLENGTH - 1); if (percentage >= 80) printf "DISK: %s, Total: %s, Avail: %s, Used: %s \n ", $1, $2, $3, $4}' | sort -h |  grep -Eiv "run"`
+
 #Percentage is 80 ;Only above 80% disk usage will print 
         echo -e "${TIME} ${IP} ALERT: DISK usage is above 80% for $disk" >> ${log}
 }
@@ -55,3 +63,4 @@ function show_disk_usage() {
 show_disk_usage
 show_ram_usage
 show_cpu_usage
+upload_to_sftp
