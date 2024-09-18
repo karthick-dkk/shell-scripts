d26adbce7bb9f6759a685b7f2b29b400460601aa
Karthick D <128692563+karthick-dkk@users.noreply.github.com>
2024-09-18 13:37:42 +0530

    Create AWS_S3_Bucket_Size_Calculation.sh

---
+++ AWS/AWS_S3_Bucket_Size_Calculation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# File containing S3 bucket names (one per line)
+aws s3 ls | awk '{print $3 }' > buckets.txt
+BUCKET_FILE="buckets.txt"
+date=`date +%Y-%m-%d`
+output_file="s3_bucket"
+
+# Use the AWS CLI to list folders (common prefixes) inside each S3 bucket
+while read -r S3_BUCKET; do
+  # List folders inside the S3 bucket
+  folders=$(aws s3 ls "s3://$S3_BUCKET/" | grep PRE | awk '{print $2}' | sort -u)
+
+  # Iterate through and print each folder name
+  for folder in $folders; do
+    # Replace slashes (/) with underscores (_) in bucket and folder names
+    sanitized_bucket_name=$(echo "$S3_BUCKET" | tr / _)
+    sanitized_folder_name=$(echo "$folder" | tr / _)
+
+    # Create filenames with sanitized names
+    bucket_filename="${sanitized_bucket_name}.txt"
+    folder_filename="${sanitized_bucket_name}_${sanitized_folder_name}.txt"
+
+    echo "s3://$S3_BUCKET/$folder"
+    # Save the output to the appropriately named files
+    #aws s3 ls "s3://$S3_BUCKET" --recursive --human-readable --summarize >> "$bucket_filename"
+    aws s3 ls "s3://$S3_BUCKET/$folder" --recursive --human-readable --summarize >> "$folder_filename"
+  done
+done < "$BUCKET_FILE" 
+####Output File
+sleep 10
+grep " Total Size:" * > {$output_file}_{$date}.log
+
+####Delete the files#####
+sleep 10
+
+grep " Total Size:" * | awk -F":" '{print$1}' > deletion
+#### Uncomment the below line for delete the log files
+sed  's/^/rm -rf /' deletion > delete_file && sh delete_file
+
+#######Script End#########
