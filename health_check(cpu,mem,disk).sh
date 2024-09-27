3c6d0e6cde78786bc0b642d481414a21708b1922
Karthick D <128692563+karthick-dkk@users.noreply.github.com>
2024-09-27 09:35:06 +0530

    Update health_check(cpu,mem,disk).sh

--- health_check(cpu,mem,disk).sh
+++ health_check(cpu,mem,disk).sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
-echo -e "Time\t\t\tMemory\t\tDisk\t\tCPU"
-end=$((SECONDS+3600))
+echo -e "Time\t\t\tMemory\t\tDisk /root\tCPU"
+seconds="3600"
+end=$((SECONDS+seconds))
 while [ $SECONDS -lt $end ]; do
 MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
