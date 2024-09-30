e1003fa4368403d1c06fa619e2b1e0bc3441a459
Karthick D <128692563+karthick-dkk@users.noreply.github.com>
2024-10-01 00:19:00 +0530

    Update health_check_linux.sh

--- health_check_linux.sh
+++ health_check_linux.sh
@@ -4,7 +4,7 @@ echo -e "Time\t\t\tMemory\t\tDisk /root\tCPU"
 seconds="3600"
 end=$((SECONDS+seconds))
 while [ $SECONDS -lt $end ]; do
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+MEMORY=$(free -m | awk 'NR==2{printf "%.f%%\t\t", $3*100/$2 }')
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 #CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf("%.f\n", 100 - $1)}')
